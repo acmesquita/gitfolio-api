870eb7651a4acd5f566f7b3fe86c25024933f5fa
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-05-16 22:07:12 -0300

    Criando seed com 10 portifólios para testes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..10).each do |p|
+  Portfolio.create({login: "login #{p}", name: "name #{p}", email: "email#{p}@email.com"})
+end
